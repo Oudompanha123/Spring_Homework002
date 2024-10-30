652a0c85e466e1f84d1802596a0e93c95b254e68
Chamreun Odompanha <oudompanha456@gmail.com>
2024-10-30 09:09:37 +0700

    My commit

--- src/main/resources/static/scheme.sql
+++ src/main/resources/static/scheme.sql
@@ -8,3 +8,11 @@ VALUES (
            '[{"color": "red", "size": "large"}, {"material": "leather", "waterproof": true}]',
            true
        );
+
+UPDATE public.product
+SET specifications = '[{"additionalProp1": {"storage": "1TB", "graphics": "4K HDR", "processor": "Custom AMD CPU"}, "additionalProp2": {"controllerIncluded": true, "onlineSubscription": "Available", "backwardCompatibility": true}, "additionalProp3": {"brand": "GameSphere", "color": "Black", "releaseDate": "2023-11-15"}}]'
+WHERE id = 18;
+
+SELECT *
+FROM product
+WHERE specifications -> 'additionalProp1' ->> 'color' = 'blue';
